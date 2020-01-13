e795a83c1480d0c88d7d6dc1f90bb898520737b1
shi00055 <shi00055@algonquinlive.com>
2020-01-13 16:11:11 -0500

    This is README.md

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = "Zhuoya Shi"
+console.log('Hello, my name is $.')
+let number = 5
+let product = number * 2
+
+console.log('The number ${number} multiplied by 2 is ${}')
+console.log('The number multiplied by 5 is ${product*5}')
+console.log('The result divided by 5 is ')
