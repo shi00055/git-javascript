370d30707f76ebf59576860c6fc1210d929a3339
shi00055 <shi00055@algonquinlive.com>
2020-01-13 16:21:45 -0500

    This is script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let name = "Zhuoya Shi"
-console.log('Hello, my name is $.')
+console.log(`Hello, my name is ${name} .`)
 let number = 5
 let product = number * 2
 
-console.log('The number ${number} multiplied by 2 is ${}')
-console.log('The number multiplied by 5 is ${product*5}')
-console.log('The result divided by 5 is ')
+console.log(`The number ${number} multiplied by 2 is ${number * 2}`)
+console.log(`The number multiplied by 5 is ${product*5}.`)
+console.log(`The result divided by 5 is ${(product * 5) / number} `)
+let finalValue = (product * 5) / number
+console.log(`The result minus 7 is ${finalValue - 7}.`)
